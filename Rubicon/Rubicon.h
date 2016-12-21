51c8a436b069752ad578393d6f6ed2692493cca5
Galen Rhodes <galenrhodes@me.com>
2016-12-21 13:37:45 -0500

    Initial Commit

---
+++ Rubicon/Rubicon.h
@@ -0,0 +1,19 @@
+//
+//  Rubicon.h
+//  Rubicon
+//
+//  Created by Galen Rhodes on 12/21/16.
+//  Copyright © 2016 Project Galen. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for Rubicon.
+FOUNDATION_EXPORT double RubiconVersionNumber;
+
+//! Project version string for Rubicon.
+FOUNDATION_EXPORT const unsigned char RubiconVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Rubicon/PublicHeader.h>
+
+
---
+++ RubiconTests/RubiconTests.m
@@ -0,0 +1,39 @@
+//
+//  RubiconTests.m
+//  RubiconTests
+//
+//  Created by Galen Rhodes on 12/21/16.
+//  Copyright © 2016 Project Galen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface RubiconTests : XCTestCase
+
+@end
+
+@implementation RubiconTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
